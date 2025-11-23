7ae7326c9d951ddf1fbe191fffb6864816f6e166
Bayu Official <anosvoldigoard02@gmail.com>
2025-11-23 21:32:24 +0700

    Create next.config.js

---
+++ backend/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://animenox-stream.netlify.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
