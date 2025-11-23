5d2bfee6f1349c6b27fffbd450bf7de1512a5d99
Bayu Official <anosvoldigoard02@gmail.com>
2025-11-23 22:02:21 +0700

    Create constants.js

---
+++ backend/utils/constants.js
@@ -0,0 +1,31 @@
+export const ANIME_GENRES = [
+  'Action', 'Adventure', 'Cars', 'Comedy', 'Dementia', 'Demons', 'Drama',
+  'Ecchi', 'Fantasy', 'Game', 'Harem', 'Historical', 'Horror', 'Josei',
+  'Kids', 'Magic', 'Martial Arts', 'Mecha', 'Military', 'Music', 'Mystery',
+  'Parody', 'Police', 'Psychological', 'Romance', 'Samurai', 'School',
+  'Sci-Fi', 'Seinen', 'Shoujo', 'Shoujo Ai', 'Shounen', 'Shounen Ai',
+  'Slice of Life', 'Space', 'Sports', 'Super Power', 'Supernatural',
+  'Thriller', 'Vampire', 'Yaoi', 'Yuri'
+]
+
+export const ANIME_STATUS = [
+  'Ongoing', 'Completed', 'Upcoming'
+]
+
+export const ANIME_TYPES = [
+  'TV', 'Movie', 'OVA', 'ONA', 'Special'
+]
+
+export const STREAMING_SERVERS = [
+  'gogocdn',
+  'streamsb',
+  'vidstreaming',
+  'mp4upload'
+]
+
+export const SORT_OPTIONS = [
+  { value: 'latest', label: 'Latest' },
+  { value: 'popular', label: 'Popular' },
+  { value: 'rating', label: 'Rating' },
+  { value: 'title', label: 'Title A-Z' }
+]
